5df76ee34d46c8297be5df6f89139620ef2548d5
MHC32 <hantzmichaelchery6@gmail.com>
2025-08-27 01:59:31 -0400

    files db update

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ console.log("Tentative de connexion à MongoDB Replica Set...");
 
 (async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27020/Miwiwi?replicaSet=rs0", {
+    await mongoose.connect("mongodb://127.0.0.1:27018/Miwiwi?replicaSet=rs0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
