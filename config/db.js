72d54b2b72ca3c9085a8da641a36bfb16d44758e
MHC32 <hantzmichaelchery6@gmail.com>
2025-09-20 23:14:58 -0400

    phase 2  is done

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ console.log("Tentative de connexion à MongoDB Replica Set...");
 
 (async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27018/Miwiwi?replicaSet=rs0");    //()  "mongodb://127.0.0.1:27018,127.0.0.1:27019,127.0.0.1:27020/Miwiwi?replicaSet=rs0",
+    await mongoose.connect("mongodb://127.0.0.1:27018,127.0.0.1:27019,127.0.0.1:27020/Miwiwi?replicaSet=rs0",);    //("mongodb://127.0.0.1:27018/Miwiwi?replicaSet=rs0")
     console.log("✅ Connecté à MongoDB Replica Set !");
   } catch (error) {
     console.error("❌ Échec de la connexion MongoDB :", error.message);
