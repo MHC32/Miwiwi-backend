28959178882567d7dde5e37bfda039ed43a87d60
MHC32 <hantzmichaelchery6@gmail.com>
2026-01-07 02:37:20 -0500

    (KES-01) feat: correction in db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-console.log("Tentative de connexion à MongoDB Replica Set...");
-
-(async () => {
+const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27018/Miwiwi?replicaSet=rs0",);    
-    console.log("✅ Connecté à MongoDB Replica Set !");
+    const conn = await mongoose.connect(
+      process.env.MONGODB_URI
+    );
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    return conn;
   } catch (error) {
-    console.error("❌ Échec de la connexion MongoDB :", error.message);
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
   }
-})();
+};
 
-
-//mongodb://127.0.0.1:27018,127.0.0.1:27019,127.0.0.1:27020/Miwiwi?replicaSet=rs0
-//("mongodb://127.0.0.1:27018/Miwiwi?replicaSet=rs0")
+module.exports = connectDB; 
