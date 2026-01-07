e09c3905744fda6da8fa758587b5095f43bcd4e8
MHC32 <hantzmichaelchery6@gmail.com>
2026-01-07 02:42:20 -0500

    (KES-01) feat: correction in db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGODB_URI
+      process.env.MONGODB_URI || "mongodb://127.0.0.1:27018/Miwiwi?replicaSet=rs0"
     );
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     return conn;
